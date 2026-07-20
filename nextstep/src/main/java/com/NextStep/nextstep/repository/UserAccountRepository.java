834c145bb1d07e4087f40dc2f4ffcd2d2d009faf
Moises-Rosario <moisesneriel2002@gmail.com>
2026-07-20 16:15:15 -0500

    Change ID type to Long and add findByEmail method
    Updated UserAccountRepository to use Long as ID type and added method to find UserAccount by email.

--- nextstep/src/main/java/com/NextStep/nextstep/repository/UserAccountRepository.java
+++ nextstep/src/main/java/com/NextStep/nextstep/repository/UserAccountRepository.java
@@ -1,8 +1,9 @@
 package com.NextStep.nextstep.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.NextStep.nextstep.entity.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Integer>{
-
+// Adding <UserAccount, Long> fixes the "incompatible types" error
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    UserAccount findByEmail(String email);
 }
