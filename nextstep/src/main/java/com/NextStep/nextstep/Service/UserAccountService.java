e66a2bda776fbea8d45408fd68ae435b64feff19
Moises-Rosario <moisesneriel2002@gmail.com>
2026-07-20 16:16:34 -0500

    Refactor UserAccountService and add loginUser method
    Refactored registerUser method to accept a UserAccount object. Added loginUser method for user authentication.

--- nextstep/src/main/java/com/NextStep/nextstep/Service/UserAccountService.java
+++ nextstep/src/main/java/com/NextStep/nextstep/Service/UserAccountService.java
@@ -1,5 +1,4 @@
 package com.NextStep.nextstep.Service;
-import java.util.Scanner;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,25 +7,28 @@ import com.NextStep.nextstep.repository.UserAccountRepository;
 
 @Service
 public class UserAccountService {
-	  private final UserAccountRepository userAccountRepository;
-	  
-	  public UserAccountService(UserAccountRepository userAccountRepository) {
-		    this.userAccountRepository = userAccountRepository;
-		}
-  
-@Transactional	  
-public UserAccount registerUser( String firstname,String lastname,String email,String password) {
-	System.out.println("New User start here: Enter Information to begin" );
-	UserAccount user= new UserAccount();
-	user.setFirstName(firstname);
-	user.setLastname(lastname);
-	user.setEmail(email);
-	user.setpassword(password);
-	
-	
-	
-	return userAccountRepository.save(user);
-}
-	
-}
 
+    private final UserAccountRepository userAccountRepository;
+
+    public UserAccountService(UserAccountRepository userAccountRepository) {
+        this.userAccountRepository = userAccountRepository;
+    }
+
+    @Transactional
+    public UserAccount registerUser(UserAccount user) {
+        System.out.println("New User start here: Enter Information to begin");
+        return userAccountRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public UserAccount loginUser(String email, String password) {
+        UserAccount user = userAccountRepository.findByEmail(email);
+
+        // Fixed getpassword() -> getPassword()
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+
+        throw new RuntimeException("Invalid email or password");
+    }
+}
