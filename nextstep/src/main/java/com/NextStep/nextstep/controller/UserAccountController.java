1ee833f0db32e8cb994465226dc356a14fcb8350
Moises-Rosario <moisesneriel2002@gmail.com>
2026-07-20 12:02:17 -0500

    Enable CORS for UserAccountController
    Added CrossOrigin annotation to allow requests from any origin.

--- nextstep/src/main/java/com/NextStep/nextstep/controller/UserAccountController.java
+++ nextstep/src/main/java/com/NextStep/nextstep/controller/UserAccountController.java
@@ -1,5 +1,6 @@
 package com.NextStep.nextstep.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,24 +8,23 @@ import com.NextStep.nextstep.Service.UserAccountService;
 import com.NextStep.nextstep.entity.UserAccount;
 
 @RestController
+// Change this line to allow any port (the asterisk *)
+@CrossOrigin(origins = "*") 
 public class UserAccountController {
-	private final UserAccountService userAccountService;
-	
-	public UserAccountController(UserAccountService userAccountService) {
-	    this.userAccountService = userAccountService;
-	}
-	
-@PostMapping("/register")
-
-public UserAccount registerUser(@RequestBody UserAccount user) {
-	
-	return userAccountService.registerUser(
-	user.getFirstname(),
-	user.getLastname(), 
-	user.getEmail(), 
-	user.getPassword());
+    
+    private final UserAccountService userAccountService;
+    
+    public UserAccountController(UserAccountService userAccountService) {
+        this.userAccountService = userAccountService;
+    }
+    
+    @PostMapping("/register")
+    public UserAccount registerUser(@RequestBody UserAccount user) {
+        return userAccountService.registerUser(
+            user.getFirstname(),
+            user.getLastname(), 
+            user.getEmail(), 
+            user.getPassword()
+        );
+    }
 }
-			
-	
-}
-
