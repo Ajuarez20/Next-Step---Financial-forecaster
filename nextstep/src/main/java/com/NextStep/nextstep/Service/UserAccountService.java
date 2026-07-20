a7e8abe57bc6a72ee54047c9d6bbb4c8efbdb1d0
Moises-Rosario <moisesneriel2002@gmail.com>
2026-07-20 11:56:15 -0500

    Add @Transactional to registerUser method
    Added @Transactional annotation to registerUser method.

--- nextstep/src/main/java/com/NextStep/nextstep/Service/UserAccountService.java
+++ nextstep/src/main/java/com/NextStep/nextstep/Service/UserAccountService.java
@@ -1,13 +1,10 @@
 package com.NextStep.nextstep.Service;
-
 import java.util.Scanner;
 
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import com.NextStep.nextstep.entity.UserAccount;
 import com.NextStep.nextstep.repository.UserAccountRepository;
-
-
 
 @Service
 public class UserAccountService {
@@ -15,8 +12,9 @@ public class UserAccountService {
 	  
 	  public UserAccountService(UserAccountRepository userAccountRepository) {
 		    this.userAccountRepository = userAccountRepository;
-		}  
-	  
+		}
+  
+@Transactional	  
 public UserAccount registerUser( String firstname,String lastname,String email,String password) {
 	System.out.println("New User start here: Enter Information to begin" );
 	UserAccount user= new UserAccount();
