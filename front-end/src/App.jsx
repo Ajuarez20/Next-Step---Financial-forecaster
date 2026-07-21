14c8adedf41568d87f50f9c74751f9fcd828972a
Moises-Rosario <moisesneriel2002@gmail.com>
2026-07-21 13:29:53 -0500

    Add routing to App component with multiple routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegistrationForm from './RegistrationForm';
+import LoginForm from './LoginForm';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <RegistrationForm />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<RegistrationForm />} />
+          <Route path="/register" element={<RegistrationForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
