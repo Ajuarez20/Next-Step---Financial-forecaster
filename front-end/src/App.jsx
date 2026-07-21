8c5e876db996356b014d652dcc3dee9c4833dd40
Moises-Rosario <moisesneriel2002@gmail.com>
2026-07-21 13:04:43 -0500

    Simplify App component by removing routing
    Removed routing and replaced it with a direct RegistrationForm component rendering.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,18 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegistrationForm from './RegistrationForm';
-import LoginForm from './LoginForm';
-import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<RegistrationForm />} />
-        <Route path="/register" element={<RegistrationForm />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <RegistrationForm />
+    </div>
   );
 }
 
