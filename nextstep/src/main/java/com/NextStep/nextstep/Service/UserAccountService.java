8d551490543ba4037f38086b8c9c6e8a9412c395
cooperglenn05 <cooperglenn05@gmail.com>
2026-07-21 19:44:44 -0500

    cooper: Hash passwords with BCrypt, hide password field in responses, add crypto dependency

--- nextstep/src/main/java/com/NextStep/nextstep/Service/UserAccountService.java
+++ nextstep/src/main/java/com/NextStep/nextstep/Service/UserAccountService.java
@@ -1,32 +1,57 @@
 package com.NextStep.nextstep.Service;
-import java.util.Scanner;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.NextStep.nextstep.entity.FinancialProfile;
 import com.NextStep.nextstep.entity.UserAccount;
 import com.NextStep.nextstep.repository.UserAccountRepository;
 
 @Service
 public class UserAccountService {
-	  private final UserAccountRepository userAccountRepository;
-	  
-	  public UserAccountService(UserAccountRepository userAccountRepository) {
-		    this.userAccountRepository = userAccountRepository;
-		}
-  
-@Transactional	  
-public UserAccount registerUser( String firstname,String lastname,String email,String password) {
-	System.out.println("New User start here: Enter Information to begin" );
-	UserAccount user= new UserAccount();
-	user.setFirstName(firstname);
-	user.setLastname(lastname);
-	user.setEmail(email);
-	user.setpassword(password);
-	
-	
-	
-	return userAccountRepository.save(user);
-}
-	
-}
 
+    private final UserAccountRepository userAccountRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public UserAccountService(UserAccountRepository userAccountRepository) {
+        this.userAccountRepository = userAccountRepository;
+    }
+
+    @Transactional
+    public UserAccount registerUser(UserAccount user) {
+        System.out.println("New User start here: Enter Information to begin");
+
+        // Automatically attach a blank profile if one isn't attached yet
+        if (user.getFinancialProfile() == null) {
+            FinancialProfile profile = new FinancialProfile();
+            profile.setMonthlyIncome(0.0);
+            profile.setMonthlyExpenses(0.0);
+            profile.setCurrentSavings(0.0);
+            profile.setTargetGoalAmount(0.0);
+            profile.setDebt(0.0);
+
+            user.setFinancialProfile(profile);
+            profile.setUserAccount(user);
+        }
+
+        // Hash the password before saving (if present)
+        if (user.getPassword() != null && !user.getPassword().isEmpty()) {
+            String hashed = passwordEncoder.encode(user.getPassword());
+            user.setPassword(hashed);
+        }
+
+        return userAccountRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public UserAccount loginUser(String email, String password) {
+        UserAccount user = userAccountRepository.findByEmail(email);
+
+        if (user != null && user.getPassword() != null && passwordEncoder.matches(password, user.getPassword())) {
+            return user;
+        }
+
+        throw new RuntimeException("Invalid email or password");
+    }
+}
--- nextstep/src/main/java/com/NextStep/nextstep/entity/UserAccount.java
+++ nextstep/src/main/java/com/NextStep/nextstep/entity/UserAccount.java
@@ -4,52 +4,77 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.JoinColumn;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class UserAccount {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-   
     private Integer id;
+
     private String firstname;
     private String lastname;
     private String email;
-    private String password;
-    
-    
-    public Integer getId() {
-    	return id;
-    }
-    public String getFirstname() {
-    	return firstname;
-    }
-    public String getLastname() {
-    	return lastname;
-    }
-    public String getEmail() {
-    	return email;
-    }
-    public String getPassword() {
-    	return password;
-    }
-    
-    public void setId(int id) {
-    	this.id=id;
-    }
-    
-    public void setFirstName(String firstname) {
-    	this.firstname=firstname;
-    }
-    public void setLastname(String lastname) {
-    	this.lastname=lastname;
-    }
-    public void setEmail(String email) {
-    	this.email=email;
-    }
-    public void setpassword(String password) {
-    	this.password=password;
-    }
-    
-}
 
+    @JsonIgnore
+    private String password;
+
+    // Connects UserAccount to FinancialProfile
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "financial_profile_id")
+    private FinancialProfile financialProfile;
+
+    // Getters
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public FinancialProfile getFinancialProfile() {
+        return financialProfile;
+    }
+
+    // Setters
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFinancialProfile(FinancialProfile financialProfile) {
+        this.financialProfile = financialProfile;
+    }
+}
